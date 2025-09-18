732033fd71791f5e52f0962d4f64f028558ed9da
Marcin Michniak <marcin.michniak@outlook.com>
2025-09-18 19:29:53 +0200

    Describe "reverse words in a string"

--- src/chapters/04-array-and-string/05-conclusion/12_reverse_words_in_a_string.py
+++ src/chapters/04-array-and-string/05-conclusion/12_reverse_words_in_a_string.py
@@ -5,20 +5,29 @@ class Solution(object):
         length = len(chars)
 
         while right < length:
+            # Skip spaces
             while right < length and chars[right] == ' ':
                 right += 1
 
+            # Move the word to the left
             while right < length and chars[right] != ' ':
                 chars[left] = chars[right]
                 left += 1
                 right += 1
 
+            # Skip trailing spaces
             while right < length and  chars[right] == ' ':
                 right += 1
-            
+
+            # Important detail: if there are more chars
+            # yet then insert single space between the words.
+            # Without it there will be no spaces between them.
+            # E.g. consider input '  a  b '. This function
+            # would return 'ab' instead of 'a b'.
             if right < length:
                 chars[left] = ' '
                 left += 1
+        # Return all characters from the beginning up to `left`
         return chars[:left]
 
     def rev(self, s, start, end):
@@ -32,10 +41,20 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        # Intuition:
+        # Using trim and split functions make the problem
+        # trivial but the desired solution is a bit
+        # more complex: 
+        # 0. convert string to char array
+        # 1. remove excessive spaces
+        # 2. reverse entire array
+        # 3. reverse each word
+
         chars = self.remove_spaces(list(s))
         
         self.rev(chars, 0, len(chars) - 1)
     
+        # reverse words
         start = 0
         for i in range(len(chars) + 1):
             if i == len(chars) or chars[i] == ' ':
