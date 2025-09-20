9a519a82795f2e55fdf17905b78217614be41c7e
Marcin Michniak <marcin.michniak@outlook.com>
2025-09-20 11:49:46 +0200

    Update doubly linked list implementation

--- src/chapters/02-linked-list/15_doubly_linked_list.py
+++ src/chapters/02-linked-list/15_doubly_linked_list.py
@@ -8,14 +8,13 @@
 # obj.addAtIndex(index,val)
 # obj.deleteAtIndex(index)
 
-class MyNode(object):
-    def __init__(self, val):
-        self.value = val
-        self.next = None
-        self.prev = None
+class Node(object):
+    def __init__(self, val, next = None, prev = None):
+        self.val = val
+        self.next = next
+        self.prev = prev
 
 class MyLinkedList(object):
-
     def __init__(self):
         self.head = None
 
@@ -24,62 +23,65 @@ class MyLinkedList(object):
         :type index: int
         :rtype: int
         """
-        i = 0
         current = self.head
-        while i <= index and current:
-            if i == index:
-                return current.value
+        i = 0
+        while i < index:
+            if current is None:
+                return -1
             current = current.next
             i += 1
-        return -1
+        return current.val if current else -1
         
+
     def addAtHead(self, val):
         """
         :type val: int
         :rtype: None
         """
-        n = MyNode(val)
-        n.next = self.head
-        if self.head:
-            self.head.prev = n
+        n = Node(val, self.head)
         self.head = n
+        if self.head.next:
+            self.head.next.prev = self.head
+        
 
     def addAtTail(self, val):
         """
         :type val: int
         :rtype: None
         """
-        if not self.head:
-            self.head = MyNode(val)
+        n = Node(val)
+        if self.head is None:
+            self.head = n
             return
         current = self.head
         while current and current.next:
             current = current.next
-        if current:
-            n = MyNode(val)
-            current.next = n
-            n.prev = current
+        current.next = n
+        n.prev = current
         
+
     def addAtIndex(self, index, val):
         """
         :type index: int
         :type val: int
         :rtype: None
         """
-        i = 0
-        current = self.head
         if index == 0:
             self.addAtHead(val)
             return
-        while current:
-            if i + 1 == index:
-                n = MyNode(val)
-                n.next = current.next
-                if n.next: # Might be `None` because we have just updated `n.next``
-                    n.next.prev = n
-                current.next = n
-            i += 1
+        i = 1
+        current = self.head
+        while i < index:
+            if current is None:
+                return
             current = current.next
+            i += 1
+        if current is None:
+            return
+        current.next = Node(val, current.next)
+        if current.next.next:
+            current.next.next.prev = current.next
+        
 
     def deleteAtIndex(self, index):
         """
@@ -88,26 +90,23 @@ class MyLinkedList(object):
         """
         if self.head is None:
             return
-
         if index == 0:
-            n = self.head.next
-            self.head.next = None
-            self.head = n
-            if n:
-                n.prev = None
+            tmp = self.head
+            self.head = self.head.next
+            tmp.next = None
             return
-        i = 0
+        i = 1
         current = self.head
-        while current:
-            if i == index:
-                # Index is larger than list length.
-                break
-            if i + 1 == index:
-                if current.next:
-                    current.next.prev = None
-                    current.next = current.next.next
-                if current.next:
-                    current.next.prev = current
+        while i < index:
+            if current is None:
+                return
+            current = current.next
             i += 1
-            current = current.next  
-        
+        if current is None or current.next is None:
+            return
+
+        tmp = current.next.next
+        current.next.next = None
+        current.next = tmp
+        if tmp:
+            tmp.prev = current
