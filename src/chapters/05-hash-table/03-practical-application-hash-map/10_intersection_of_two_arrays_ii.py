41db7a86e4dd97481460f08ea0fc39ca7d37fb2b
Marcin Michniak <marcin.michniak@outlook.com>
2025-10-10 22:05:34 +0200

    Intersection of Two Arrays II

---
+++ src/chapters/05-hash-table/03-practical-application-hash-map/10_intersection_of_two_arrays_ii.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1178/
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        # Intuition:
+        # Count occurrences of numbers in nums1.
+        # Iterate the second list and append `n`
+        # as long as it's in the map and mapped
+        # avlue is higher than 0.
+        freq = {}
+        for n in nums1:
+            freq[n] = freq.get(n, 0) + 1
+        
+        res = []
+
+        for n in nums2:
+            if n in freq and freq[n] > 0:
+                res.append(n)
+                freq[n] -= 1
+        return res
+            
