518bf5e8b74020f182d1e56db64e6eadbf10b230
Marcin Michniak <marcin.michniak@outlook.com>
2025-12-20 12:21:42 +0100

    Add description for "Copy a list with random pointer" and add alternative solution

--- src/chapters/02-linked-list/04-conclusion/13_copy_a_list_with_random_pointer.py
+++ src/chapters/02-linked-list/04-conclusion/13_copy_a_list_with_random_pointer.py
@@ -12,6 +12,15 @@ class Solution(object):
         :type head: Node
         :rtype: Node
         """
+        # Intuition:
+        # Build a mapping of original nodes to clones.
+        # Then use the mapping to set random nodes in the
+        # cloned linked list.
+        # Alternatively:
+        # Iterate the list three times:
+        # 1. clone each node
+        # 2. assign random nodes for cloned nodes
+        # 3. separate original and cloned nodes
         mapping = {}
         currentOriginal = head
         dummyHead = Node(0)
@@ -36,3 +45,32 @@ class Solution(object):
             currentNew = currentNew.next
         
         return dummyHead.next
+
+    def copyRandomListAlt(self, head):
+            """
+            :type head: Node
+            :rtype: Node
+            """
+            if head is None:
+                return None
+            c = head
+            while c:
+                t = c.next
+                c.next = Node(c.val)
+                c.next.next = t
+                c = c.next.next
+            c = head
+            while c and c.next:
+                if c.random:
+                    c.next.random = c.random.next
+                c = c.next.next
+            c = head
+            cc = head.next
+            res = head.next
+            while c and c.next:
+                c.next = c.next.next
+                if cc.next:
+                    cc.next = cc.next.next
+                c = c.next
+                cc = cc.next
+            return res
